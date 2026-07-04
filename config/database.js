8a74f83b20acdd4087e6d2e10e8b4d9cb302888f
GuadalupeVeliz <guadalupeveliz100@gmail.com>
2026-07-04 15:29:45 -0300

    se agrega supabase

--- config/database.js
+++ config/database.js
@@ -1,15 +1,19 @@
-const {Sequelize} =require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('tpweb','postgres','web2026',
-    {
-        host: 'localhost',
-        dialect : 'postgres',
-        logging : false
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
-sequelize.authenticate()
-.then( () => console.log('DB is connected to PostgreSQL'))
-.catch( error => console.log ( 'Error al conectar a PostgreSQL:', error) );
+sequelize
+  .authenticate()
+  .then(() => console.log("DB is connected to PostgreSQL"))
+  .catch((error) => console.log("Error al conectar a PostgreSQL:", error));
 
-module.exports= sequelize;
+module.exports = sequelize;
